24cd98e2d030385bf422bf22a878bb171441d3d9
Walter Rosales <Walter Rosales@AFI-WR-30>
2023-09-27 20:54:57 -0600

    Carga Inicial, pantalla login (sin funcionalidades y pantalla vacia)

---
+++ src/erp/sistema/ErpSistema.java
@@ -0,0 +1,22 @@
+
+package erp.sistema;
+
+/**
+ *
+ * @author Walter Rosales
+ */
+public class ErpSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        login inicio = new login();
+        
+        inicio.setVisible(true);
+        
+    }
+    
+}

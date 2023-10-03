77af3db919ddb3facba8ea3b43dfbea881c9a96b
Walter Rosales <Walter Rosales@AFI-WR-30>
2023-10-02 23:24:30 -0600

    Agrego clase de usuarios

---
+++ src/erp/sistema/Usuarios.java
@@ -0,0 +1,44 @@
+
+package erp.sistema;
+
+
+public class Usuarios {
+    private String username;
+    private String clave;
+    private String nombres_completos;
+    
+    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombres_completos() {
+        return nombres_completos;
+    }
+
+    public void setNombres_completos(String nombres_completos) {
+        this.nombres_completos = nombres_completos;
+    }
+    
+    public int Autenticacion(String usurname, String clave){
+    
+    
+        return 0;
+    
+    
+    };
+
+}
